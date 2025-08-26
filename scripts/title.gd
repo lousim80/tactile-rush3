4e0d611e4dbc3f8f7b7744aaec0be029ddb379d4
lousim80 <lousim80@kingshigh.school.nz>
2025-08-26 12:26:12 +1200

    hi

--- scripts/title.gd
+++ scripts/title.gd
@@ -3,8 +3,8 @@ extends Control
 func _ready():
 	GlobalSettings.apply_color_scheme(get_tree().root)
 	
-	$VBoxContainer/Button.text = "Start Game"
-	$VBoxContainer/Button.pressed.connect(_on_start_game_pressed)
+	$VBoxContainer/Button.text = "Accessibility"
+	$VBoxContainer/Button.pressed.connect(_on_accessibility_pressed)
 
 	$VBoxContainer/Button2.text = "Level Select"
 	$VBoxContainer/Button2.pressed.connect(_on_level_select_pressed)
@@ -12,9 +12,14 @@ func _ready():
 	$VBoxContainer/Button3.text = "Quit"
 	$VBoxContainer/Button3.pressed.connect(_on_quit_pressed)
 
+	$VBoxContainer/Button4.text = "Tutorial"
+	$VBoxContainer/Button4.pressed.connect(_on_tutorial_pressed)
 
-func _on_start_game_pressed():
-	get_tree().change_scene_to_file("res://main scenes/menus/main_menu.tscn")
+func _on_accessibility_pressed():
+	get_tree().change_scene_to_file("res://main scenes/menus/accessibility_menu.tscn")
+
+func _on_tutorial_pressed():
+	get_tree().change_scene_to_file("res://main scenes/menus/tutorial_scene.tscn")
 
 func _on_level_select_pressed():
 	get_tree().change_scene_to_file("res://main scenes/menus/level_select.tscn")
